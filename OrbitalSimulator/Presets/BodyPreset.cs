eca4d50238d18c323e763a8a8472d3588b353885
thomaswening <thomaswening94@gmail.com>
2022-12-01 01:21:00 +0100

    add preset models

---
+++ OrbitalSimulator/Presets/BodyPreset.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbitalSimulator.Presets
+{
+    internal class BodyPreset
+    {
+
+        #region Fields
+
+        private readonly double[] initialPosition = new double[3];
+        private readonly double[] initialVelocity = new double[3];
+        private double mass;
+
+        #endregion Fields
+
+        #region Properties
+
+        public bool IsFixed { get; set; }
+        public bool IsMassive { get; set; }
+        public double Mass 
+        { 
+            get
+            {
+                if (IsMassive) return Mass;
+                return 0;
+            }
+
+            set { mass = value; }
+        }
+
+        public string Name { get; set; } = string.Empty;
+        public double[] InitialPosition => initialPosition;
+        public double[] InitialVelocity
+        {
+            get
+            {
+                if (IsFixed) return new double[] { 0, 0, 0 };
+                return initialVelocity;
+            }
+        }
+
+        #endregion Properties
+
+        #region Public Methods
+
+        public void SetInitialPosition(double x, double y, double z)
+        {
+            SetInitialComponents(initialPosition, x, y, z);
+        }
+
+        public void SetInitialVelocity(double x, double y, double z)
+        {
+            SetInitialComponents(initialVelocity, x, y, z);
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        static void SetInitialComponents(double[] vector, double x, double y, double z)
+        {
+            vector[0] = x; 
+            vector[1] = y;
+            vector[2] = z;
+        }
+
+        #endregion Private Methods
+
+    }
+}
---
+++ OrbitalSimulator/Presets/SimulationPreset.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OrbitalSimulator.Presets;
+
+namespace OrbitalSimulator.Models
+{
+    internal class SimulationPreset
+    {
+
+        #region Fields
+
+        readonly List<BodyPreset> bodies = new();
+
+        #endregion Fields
+
+        #region Properties
+
+        public string Name { get; set; }
+
+        public string IntegrationType { get; set; }
+
+        public double TimeSpan { get; set; }
+
+        public double TimeResolution { get; set; }
+
+        public List<BodyPreset> Bodies => bodies;
+
+        #endregion Properties
+
+        #region Public Constructors
+
+        public SimulationPreset(string name, string integrationType, double timeSpan, double timeResolution)
+        { 
+            Name = name;
+            IntegrationType = integrationType;
+            TimeSpan = timeSpan;
+            TimeResolution = timeResolution;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        public void AddBody(BodyPreset body) => bodies.Add(body);
+        public void AddBodies(List<BodyPreset> bodies) => this.bodies.AddRange(bodies);
+
+        #endregion Public Methods
+
+    }
+}
